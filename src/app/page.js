c2be94a9e630abeeacbdc4b1b35fc0b8843aa12b
Brook <felekebrook05@gmail.com>
2023-10-31 17:43:39 +0300

    installed sharp package

--- src/app/page.js
+++ src/app/page.js
@@ -14,49 +14,6 @@ export default function Home() {
   useEffect(() => {}, []);
   return (
     <>
-      {/* <nav className="border-gray-200 ">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              {buttonTitles.map((title) => (
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                    aria-current="page"
-                  >
-                    {title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </nav> */}
       <nav className="flex flex-col sm:w-full mx-auto bg-amber-950 sm:bg-inherit sm:flex-row justify-center align-middle border-2 border-solid border-slate-50 items-center text-white py-3 px-4  max-w-screen-xl sm:px-8 font-avenir ">
         <div className="flex flex-row mx-auto w-full justify-between items-center sm:hidden">
           <a href="#" className="sm:hidden">
@@ -144,16 +101,17 @@ export default function Home() {
               <img
                 className="w-full rounded-2xl brightness-50"
                 src={"/second/second_section_photo.png"}
-                alt=""
-              />
+                alt="pretty"
+              ></img>
               <img
                 src={"/first/flowers_left.png"}
                 className="w-20 lg:w-56 absolute top-0 right-0 transform translate-x-[45%] translate-y-[-30%]"
-                height={100}
+                alt="flower"
               ></img>
               <img
                 src={"/first/flowers_right.png"}
                 className="w-20 lg:w-56 absolute bottom-0 left-0 transform translate-x-[-45%] translate-y-[20%]"
+                alt="flower"
               ></img>
             </div>
           </div>
